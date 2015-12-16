706b514f82ccb2a18ab68c296da3e28c0af94c4e
Ryan Douglas <ryan@luckydogdigital.com>
2015-12-16 13:26:07 -0700

    working on pg roles

---
+++ app.js
@@ -0,0 +1,17 @@
+var pg = require('pg');
+var connectionString = 'postgres://jim:secret@localhost/roleslesson';
+pg.connect(connectionString, function (err, client, done) {
+  if (err) {
+    console.log(err);
+    done();
+    return;
+  }
+  client.query('SELECT * FROM finance', function(err, results) {
+    done();
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(results);
+  })
+});
